cb1f0fd356f63195d18f480800ac4905942954e7
Garimathaku <garimathakur397@gmail.com>
2025-02-21 15:00:01 +0530

    [Garima]Add.code for hello message app

---
+++ src/main/java/com/uc/hello_app/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.uc.hello_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Bridgelabz";
+    }
+
+
+
+
+}
+
