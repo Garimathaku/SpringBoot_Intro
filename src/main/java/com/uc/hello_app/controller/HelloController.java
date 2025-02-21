3687566bea8cdd82b8e83fc44b886bc81a57369f
Garimathaku <garimathakur397@gmail.com>
2025-02-21 16:38:12 +0530

    [Garima]Add.Hello with first and last name

--- src/main/java/com/uc/hello_app/controller/HelloController.java
+++ src/main/java/com/uc/hello_app/controller/HelloController.java
@@ -1,24 +1,33 @@
 package com.uc.hello_app.controller;
 
+import com.uc.hello_app.DTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
+    //method to show hello from bridgelabz
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from Bridgelabz";
     }
-
+ //method to show hello with name
     @GetMapping("/hello/query")
     public String sayHelloName(@RequestParam String name) {
         return "Hello " + name;
     }
     @GetMapping("/param/{name}")
-    public String sayHello(@PathVariable String name) {
+    public String sayHelloPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+
+    @PostMapping("/post")
+    public String sayHelloRequestBody(@RequestBody DTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
+     
 
 
 
