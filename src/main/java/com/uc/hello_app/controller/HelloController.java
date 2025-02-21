1823e67449c07bb3b10568503bc8aecc7195ae80
Garimathaku <garimathakur397@gmail.com>
2025-02-21 16:50:55 +0530

    [Garima]Add.Hello with put mapping

--- src/main/java/com/uc/hello_app/controller/HelloController.java
+++ src/main/java/com/uc/hello_app/controller/HelloController.java
@@ -27,9 +27,10 @@ public class HelloController {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 
-     
-
-
+    @PutMapping("/put/{firstName}")
+    public String updateHello(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
 
 
 }
